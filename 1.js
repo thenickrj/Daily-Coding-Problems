c9243938b77b85925edab471522ab707fab190b2
Nikhil Singh <nikhilbangalor@gmail.com>
2022-11-13 23:26:00 +0530

    1st

---
+++ 1.js
@@ -0,0 +1,23 @@
+//  1 -Level Easy
+
+/*
+This problem was recently asked by Google.
+
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+*/
+
+function list_num() {
+  let list = [10, 15, 3, 7];
+  let k = 17;
+  for (let i = 0; i < list.length; i++) {
+    for (let j = i + 1; j < list.length; j++) {
+      if (list[i] + list[j] === k) {
+        return [list[i], list[j]];
+      }
+    }
+  }
+}
+
+console.log(list_num());
