d5bf4adc20a93abbd1e77cbc332e71785ee0b500
Nikhil Singh <nikhilbangalor@gmail.com>
2022-11-14 20:11:01 +0530

    first

--- 1.js
+++ 1.js
@@ -11,13 +11,25 @@ For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 function list_num() {
   let list = [10, 15, 3, 7];
   let k = 17;
-  for (let i = 0; i < list.length; i++) {
-    for (let j = i + 1; j < list.length; j++) {
-      if (list[i] + list[j] === k) {
-        return [list[i], list[j]];
-      }
+  let dict = {};
+  let map = new Map();
+  for (let ele of list) {
+    if (Object.values(dict).includes(ele)) {
+      return [ele, k - ele];
+    } else if (!(ele in dict)) {
+      dict[ele] = k - ele;
+    } else {
+      dict[ele] = k - ele;
     }
   }
+
+  //   for (let i = 0; i < list.length; i++) {
+  //     for (let j = i + 1; j < list.length; j++) {
+  //       if (list[i] + list[j] === k) {
+  //         return [list[i], list[j]];
+  //       }
+  //     }
+  //   }
 }
 
 console.log(list_num());
