f132acfb6a5085f8946fdb80fe357d815a7c7a1f
Nikhil Singh <nikhilbangalor@gmail.com>
2022-11-15 19:25:37 +0530

    2

--- 2.js
+++ 2.js
@@ -14,18 +14,31 @@ Follow-up: what if you can't use division?
 
 function second(arr) {
   let new_arr = [];
-  for (let i = 0; i < arr.length; i++) {
-    let sum = 1;
-    for (let j = 0; j < arr.length; j++) {
-      if (j != i) {
-        sum *= arr[j];
-      }
-    }
-    new_arr[i] = sum;
-  }
+
+  let sum = arr.sum();
+  // for (let i = 0; i < arr.length; i++) {
+  //   let sum = 1;
+  //   for (let j = 0; j < arr.length; j++) {
+  //     if (j != i) {
+  //       sum *= arr[j];
+  //     }
+  //   }
+  //   new_arr[i] = sum;
+  // }
+
+  arr.map((ele, i) => (new_arr[i] = sum / ele));
+
   return new_arr;
 }
 
-// let arr = [1, 2, 3, 4, 5];
-let arr = [3, 2, 1];
+Array.prototype.sum = function () {
+  let prod = 1;
+  for (let i = 0; i < this.length; i++) {
+    prod *= this[i];
+  }
+  return prod;
+};
+
+let arr = [1, 2, 3, 4, 5];
+// let arr = [3, 2, 1];
 console.log(second(arr));
